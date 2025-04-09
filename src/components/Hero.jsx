91856ac70f2da477319d360897ae18debf694bb5
Kanchana K. <kanchanak.bandara@gmail.com>
2025-04-10 02:13:34 +0530

    made hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,17 @@ import NavBar from "./NavBar";
 
 function Hero() {
   return (
-    <header className="flex min-h-screen flex-col bg-[url(/public/images/mobile/image-hero.jpg)] bg-cover p-6">
-      <NavBar />
+    <header className="bg-[url(/public/images/mobile/image-hero.jpg)] bg-cover p-6 md:bg-[url(/public/images/desktop/image-hero.jpg)] md:p-0">
+      <div className="mx-auto flex min-h-screen max-w-6xl flex-col">
+        <NavBar />
 
-      <section className="flex grow items-center">
-        <h1 className="p-4 text-4xl font-light text-white uppercase ring-2 ring-white">
-          Immersive experiences <br /> that <br /> deliver
-        </h1>
-      </section>
+        <section className="flex grow items-center">
+          <h1 className="min-h-fit p-4 text-4xl font-light text-white uppercase ring-2 ring-white md:w-6/10 md:p-8 md:text-7xl">
+            Immersive <br className="hidden md:block" /> experiences <br /> that
+            <br className="md:hidden" /> deliver
+          </h1>
+        </section>
+      </div>
     </header>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,25 +13,50 @@ function NavBar() {
     [isMenuShown];
 
   return (
-    <nav className="flex items-center justify-between">
+    <nav className="flex items-center justify-between md:mt-12">
       <img
         src="/public/images/logo.svg"
         alt="loopstudios logo"
-        className="z-50 w-40"
+        className="z-50 w-40 md:w-50"
       />
 
-      <motion.img
-        src={
-          isMenuShown
-            ? "/public/images/icon-close.svg"
-            : "/public/images/icon-hamburger.svg"
-        }
-        alt="open-menu"
-        className="z-50"
-        onClick={toggleMenu}
-        animate={{ rotate: isMenuShown ? 180 : 0 }}
-        transition={{ ease: "linear" }}
-      />
+      <div className="z-50">
+        <motion.img
+          src={
+            isMenuShown
+              ? "/public/images/icon-close.svg"
+              : "/public/images/icon-hamburger.svg"
+          }
+          alt="open-menu"
+          className="md:hidden"
+          onClick={toggleMenu}
+          animate={{ rotate: isMenuShown ? 180 : 0 }}
+          transition={{ ease: "linear" }}
+        />
+
+        <ul className="hidden items-center gap-4 text-white md:flex">
+          <li className="group relative cursor-pointer">
+            About
+            <div className="absolute -bottom-4 left-1/2 my-2 h-0.5 w-8 -translate-x-1/2 bg-white opacity-0 transition-opacity duration-75 ease-in-out group-hover:opacity-100"></div>
+          </li>
+          <li className="group relative cursor-pointer">
+            Careers
+            <div className="absolute -bottom-4 left-1/2 my-2 h-0.5 w-8 -translate-x-1/2 bg-white opacity-0 transition-opacity duration-75 ease-in-out group-hover:opacity-100"></div>
+          </li>
+          <li className="group relative cursor-pointer">
+            Events
+            <div className="absolute -bottom-4 left-1/2 my-2 h-0.5 w-8 -translate-x-1/2 bg-white opacity-0 transition-opacity duration-75 ease-in-out group-hover:opacity-100"></div>
+          </li>
+          <li className="group relative cursor-pointer">
+            Products
+            <div className="absolute -bottom-4 left-1/2 my-2 h-0.5 w-8 -translate-x-1/2 bg-white opacity-0 transition-opacity duration-75 ease-in-out group-hover:opacity-100"></div>
+          </li>
+          <li className="group relative cursor-pointer">
+            Support
+            <div className="absolute -bottom-4 left-1/2 my-2 h-0.5 w-8 -translate-x-1/2 bg-white opacity-0 transition-opacity duration-75 ease-in-out group-hover:opacity-100"></div>
+          </li>
+        </ul>
+      </div>
 
       <AnimatePresence>
         {isMenuShown && (
